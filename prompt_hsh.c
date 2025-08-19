fd61c27b9144ab87a17c10a4d6d2a6828672df4c
Thomas <decourttthomas@gmail.com>
2025-08-19 14:17:15 +0200

    uptade prompt

--- prompt_hsh.c
+++ prompt_hsh.c
@@ -7,25 +7,27 @@
  */
 char *read_and_clean_line(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
-    if (isatty(STDIN_FILENO))
-    {
-        write(STDOUT_FILENO, "$ ", 2);
-    }
+	/* Only show prompt in interactive mode */
+	if (isatty(STDIN_FILENO))
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+	}
 
-    nread = getline(&line, &len, stdin);
-    if (nread == -1)
-    {
-        if (line)
-            free(line);
-        return (NULL);
-    }
+	nread = getline(&line, &len, stdin);
+	if (nread == -1)
+	{
+		if (line)
+			free(line);
+		return (NULL);
+	}
 
-    if (nread > 0 && line[nread - 1] == '\n')
-        line[nread - 1] = '\0';
+	/* Remove trailing newline */
+	if (nread > 0 && line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
 
-    return (line);
+	return (line);
 }
